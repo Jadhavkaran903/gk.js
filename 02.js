f628ac147913f49955e3cfb0c0435279bfb19103
paradox903 <jadhavkaran903@gmail.com>
2022-02-15 00:34:56 +0530

    new

---
+++ 02.js
@@ -0,0 +1,85 @@
+// function declararion
+
+// function cal(){
+
+//     console.log(4+6)
+// }
+// cal()
+
+// let yy= function add(x,y){
+
+//     return x+y
+// }
+// let uu = yy(4,8)
+// console.log(uu)
+
+// let hh = (x,y)=>{
+//     console.log(x/y)
+//         return x-y
+    
+// }
+
+// let jj =hh(10,2)
+// console.log(jj)
+
+// let kk = (x,y)=> x%y
+
+// let gg = kk(20,3)
+// console.log(gg)
+
+// // Print all even numbers from 0 – 10
+// let yyyy = []
+// for (let i = 0; i<=10; i++){
+
+//     if(i%2==0){
+//      yyyy.push(i)
+//     }
+// }
+
+// console.log(yyyy)
+
+// // Calculate the sum of numbers within an array
+
+// let hh = [23,34,43,65,76,76,87]
+// let sum = 0
+//  for(let i = 0; i<hh.length;i++){
+//   if(hh[i]){
+//       sum = sum + hh[i]
+//   }
+
+//  }
+//  console.log(sum)
+
+//  let ff = hh.reduce((acc,el)=>{
+//      return acc +el
+//  },0)
+//  console.log(ff)
+
+//  Create a function that reverses an array
+
+// let  ll =[]
+//  for (let i =0;i<hh.length;i++){
+   
+
+//         ll.unshift(hh[i])
+    
+//  }
+
+//  console.log(ll)
+
+//  for (let i=hh.length-1;i>=0;i--){
+//        ll.push(hh[i])
+  
+// }
+//  console.log(ll)    
+
+
+// //  Print a table containing multiplication tables
+//  let n = 7;
+
+//  for (let i =1;i<=10;i++){
+
+//     console.log(n+"*"+i+"="+n*i)
+//  }  
+
+ 
---
+++ nasn.js
@@ -0,0 +1,68 @@
+// https://docs.google.com/spreadsheets/d/1Ie3__Cli0LgyCt_AiLdSoIsyWkxbTYUG9Ze4sd9dGAE/edit?usp=sharing
+// Projects  Details
+
+
+// My project is web based application to book flight tickets ,
+
+// // The front is developed in react js and back is python - Django.
+
+// There is integration with many third party API, for booking  and 
+
+// Payment .
+
+// My responsibility is to test UI , API 
+
+// We once the featured is developed we test the system manually,
+
+// If bug is we report on jira and follow bug life cycle,
+
+// After and feature is deployed we look  to automate test cases for 
+
+// regression system  using  cypress
+
+
+
+
+
+// My project is web based application to book flight tickets ,
+
+// The front is developed in react js and back is python - Django.
+
+// There is integration with many third party API, for booking  and 
+
+// Payment .
+
+// My responsibility is to test UI , API 
+
+// We once the featured is developed we test the system manually,
+
+// If bug is we report on jira and follow bug life cycle,
+
+// After and feature is deployed we look  to automate test cases for 
+
+// regression system  using  cypress
+// and close , 
+
+// We have daily scrum meeting to discuss one  on current status and blocker for 15 minutes 
+
+// At end of sprint we have spring review decision
+
+
+
+
+// imp
+// userpoint == working hours 
+//  somewhere there will be 2 userponit = 1 day
+// or 1 user point = 1 day = 8 hours
+
+// burn up char and burn down chart depend on user point 
+// finish            remaining
+
+
+// velocity chart       
+
+
+
+
+console.log("hello")
+console.log("/hello/")
